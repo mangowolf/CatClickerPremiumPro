371018bbebcf6da915abe1476bb52c94ae5c5c6d
mangowolf <jimmyz949@hotmail.com>
2016-07-12 21:58:00 -0700

    Update each individual cat feature working

--- js/app.js
+++ js/app.js
@@ -91,6 +91,7 @@ var catView = {
 		this.countElem.textContent = currentCat.clickCount;
 		this.catNameElem.textContent = currentCat.name;
 		this.catImageElem.src = currentCat.imgSrc;
+
 	}
 };
 
@@ -134,6 +135,7 @@ var catListView = {
 var adminView = {
 
 	init: function(){
+		//this.newCatName = document.getElementById("new-cat-name");
 
 		this.toggle();
 		this.save();
@@ -155,15 +157,23 @@ var adminView = {
 	},
 
 	save: function(){
-		var curCat = octopus.getCurrentCat();
-		var saveButton = document.getElementById("save-button");
+		var curCat;
+		//var saveButton = document.getElementById("save-button");
+		//console.log(this.newCatName.value);
+		//this.newCatName = document.getElementById("new-cat-name");
+		this.saveButton = document.getElementById("save-button");
 		var newCatName = document.getElementById("new-cat-name");
 		var newCatURL = document.getElementById('new-cat-url');
 		var newClickNum = document.getElementById('new-click-num');
-		saveButton.addEventListener('click', function(){
+		this.saveButton.addEventListener('click', function(){
+				//console.log(this.newCatName);
+				//curCat.name = this.newCatName;
+				//this.newCatName = curCat.name;
+			curCat = octopus.getCurrentCat();
 			curCat.name = newCatName.value;
-			curCat.imgSrc = newCatURL.value;
-			curCat.clickCount = newClickNum.value;
+			console.log(curCat);
+			//curCat.imgSrc = newCatURL.value;
+			//curCat.clickCount = newClickNum.value;
 			catListView.render();
 			catView.render();
 			//var listItem = document.createElement('li');
